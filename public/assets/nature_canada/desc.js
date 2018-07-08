58ee065c54330ff42bf061fc46f57373a0eafae2
Mykola Boiko <mboiko@luxoft.com>
2018-07-08 16:55:38 +0300

    cleanup

--- public/assets/nature_canada/desc.js
+++
@@ -1,4 +0,0 @@
-const DESCRIPTION = `Is there an owl in your neighbourhood? Probably. Some can be found almost anywhere with trees, even in the city! Even so, owls are famously mysterious. Many people have never seen one in the wild. Winter is a great time to look because the bare branches help reveal roosts. Some owls such as the Great Horned and Northern Saw-Whet also become more vocal in late winter as breeding season approaches.`;
-
-const NAME = 'Tree owl';
-export default {NAME, DESCRIPTION};
